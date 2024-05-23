c7c5b8257a3e2f8b98c5cabe7c670dcece8d26af
Andrew Vargas <82424668+AndrewVargas1991@users.noreply.github.com>
2024-05-23 17:35:51 -0300

    Update Jogo de adivinhação.py

--- Jogo de adivinhação.py
+++ Jogo de adivinhação.py
@@ -1,13 +1,16 @@
 from random import randrange
 
 n = randrange(1000)
+t = 0
 print('Digite um numero entre 0 e 1000: ', end='')
 
 while True:
 	v = int(input())
 	if n == v:
-		print('Voce ACERTOU!')
+		t += 1
+		print(f'Voce ACERTOU na {t}ª tentativa!')
 		break
-	print('MENOR, digite outro numero: ' if (n < v) else ('MAIOR, digite outro numero: '))
+	print('MENOR, digite outro numero: ' if (n < v) else ('MAIOR, digite outro numero: '), end='')
+	t += 1
 
 input('\nAperte ENTER para sair...')
